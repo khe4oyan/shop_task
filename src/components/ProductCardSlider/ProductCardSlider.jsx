0db205781d4e775598c6634a3fd58613a8cb4e7c
khc <khe4oyan@gmail.com>
2024-12-07 21:15:45 +0400

    ,

--- src/components/ProductCardSlider/ProductCardSlider.jsx
+++ src/components/ProductCardSlider/ProductCardSlider.jsx
@@ -1,10 +1,36 @@
+// libs
+import { useState } from 'react';
+
 // styles
 import classes from './styles.module.css';
 
 export default function ProductCardSlider({ img }) {
+  const [slideInd, setSlideInd] = useState(0);
+
+  const prevSlideInd = () => {
+    setSlideInd(prev => {
+      return prev - 1 < 0 ? (img.length - 1) : prev - 1;
+    });
+  }
+
+  const nextSlideInd = () => {
+    setSlideInd(prev => {
+      return prev + 1 < img.length ? prev + 1 : 0;
+    });
+  }
+
   return (
     <div className={classes.root}>
-      <img src={img[0]} alt="product_banner" className={classes.img}/>
+      <img src={img[slideInd]} alt="product_banner" className={classes.img}/>
+      <div className={classes.buttons}>
+        <div className={classes.buttonBox}>
+          <button className={classes.button} onClick={prevSlideInd}>{"<"}</button>
+        </div>
+
+        <div className={classes.buttonBox}>
+          <button className={classes.button} onClick={nextSlideInd}>{">"}</button>
+        </div>
+      </div>
     </div>
   )
 }
